92062f62eaa48137dd18615f27b1f40efd1f0af8
Akshay Easwaran <akeaswaran@me.com>
2022-02-23 00:10:43 -0500

    install devtools before use

--- update.R
+++ update.R
@@ -1,6 +1,6 @@
 ### Update master schedule/play-by-play and push to GitHub repo
+install.packages(c("devtools","glue","optparse"))
 devtools::install_github(repo = "JackLich10/gamezoneR")
-install.packages(c("glue","optparse"))
 suppressMessages(suppressWarnings(library(gamezoneR)))
 suppressMessages(suppressWarnings(library(glue)))
 suppressMessages(suppressWarnings(library(optparse)))
